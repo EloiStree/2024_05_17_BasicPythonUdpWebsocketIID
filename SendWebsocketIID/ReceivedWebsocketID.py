093db715f6e8ff6c125067a3bf3bcb25bb564378
Éloi Strée <eloistree@gmail.com>
2024-05-18 17:20:50 +0200

    Update ReceivedWebsocketID.py

--- SendWebsocketIID/ReceivedWebsocketID.py
+++ SendWebsocketIID/ReceivedWebsocketID.py
@@ -10,14 +10,13 @@ debug_received_data = False
 
 def handle_integer_received(integer):
     print(f"Received integer: {integer}")
-    #Ajouter votre code ici
-    print("Ajouter votre code ici")
+    # Add your code handler here.
+
+
 
 
 
 ################### Websocket don't touch  #####################
-
-
 async def handle_message(websocket, path):
     async for message in websocket:
         message_length = len(message)
@@ -32,19 +31,15 @@ async def handle_message(websocket, path):
             if debug_received_data:
                 print(f"Received values: {value1}, {value2} (format: <iQ)")
             handle_integer_received(int(value1))
-        
         elif message_length == 16:
             value1, value2, value3 = struct.unpack('<iiQ', message)
             if debug_received_data:
                 print(f"Received values: {value1}, {value2}, {value3} (format: <iiQ)")
             handle_integer_received(int(value1))
         
-        
-
 start_server = websockets.serve(handle_message, "localhost", 7073)
-
-
 
 asyncio.get_event_loop().run_until_complete(start_server)
 print("WebSocket server is listening on port 7073")
 asyncio.get_event_loop().run_forever()
+
